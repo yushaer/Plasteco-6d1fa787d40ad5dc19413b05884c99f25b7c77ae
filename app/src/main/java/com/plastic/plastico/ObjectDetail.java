4f553a8cdc67a3e9090ea727956aa06aa3179280
apollofortytwo <32778577+BryanHuynh@users.noreply.github.com>
2019-02-16 20:40:36 -0700

    added cancel and retake in object details

--- app/src/main/java/com/plastic/plastico/ObjectDetail.java
+++ app/src/main/java/com/plastic/plastico/ObjectDetail.java
@@ -1,11 +1,16 @@
 package com.plastic.plastico;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
+
+import com.google.firebase.FirebaseApp;
+
 
 public class ObjectDetail extends AppCompatActivity {
 
@@ -15,6 +20,40 @@ public class ObjectDetail extends AppCompatActivity {
         setContentView(R.layout.activity_object_detail);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+        FirebaseApp.initializeApp(this);
 
+
+
+/*       FirebaseDatabase database = FirebaseDatabase.getInstance();
+        DatabaseReference myRef = database.getReference("ID");
+        myRef.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                // This method is called once with the initial value and again
+                // whenever data at this location is updated.
+                String value = dataSnapshot.getValue(String.class);
+                System.out.println(value);
+            }
+
+            @Override
+            public void onCancelled(DatabaseError error) {
+                // Failed to read value
+                System.out.println(error.toException());
+            }
+        });
+        */
     }
+
+    public void cancelAction(View view){
+        Intent intent = new Intent(ObjectDetail.this, MainActivity.class);
+        startActivity(intent);
+    }
+
+
+    public void retakeAction(View view){
+        Intent intent = new Intent(ObjectDetail.this, getbarcode.class);
+        startActivity(intent);
+    }
+
+
 }
