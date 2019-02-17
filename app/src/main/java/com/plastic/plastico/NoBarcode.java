7186693a201634f9e8ddac8564df16febe8b17be
Sekai422 <36085216+Sekai422@users.noreply.github.com>
2019-02-16 22:58:40 -0700

    no object barcode button on MainActivity

--- app/src/main/java/com/plastic/plastico/NoBarcode.java
+++ app/src/main/java/com/plastic/plastico/NoBarcode.java
@@ -3,15 +3,11 @@ package com.plastic.plastico;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class noObjectBarcode extends AppCompatActivity {
+public class NoBarcode extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_no_object_barcode);
-
-
+        setContentView(R.layout.activity_no_barcode);
     }
-
-
 }
