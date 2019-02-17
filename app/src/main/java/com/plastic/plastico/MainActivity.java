f7b80e2f1ef3a3b222a820be2aec03f8343fd091
yushaefraza@gmail.com <yushaefraza@gmail.com>
2019-02-16 20:27:02 -0700

    plz work damit work

--- app/src/main/java/com/plastic/plastico/MainActivity.java
+++ app/src/main/java/com/plastic/plastico/MainActivity.java
@@ -8,6 +8,8 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.content.Intent;
 
+import com.google.firebase.FirebaseApp;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
@@ -16,7 +18,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-
+        FirebaseApp.initializeApp(this);
 
     }
 
