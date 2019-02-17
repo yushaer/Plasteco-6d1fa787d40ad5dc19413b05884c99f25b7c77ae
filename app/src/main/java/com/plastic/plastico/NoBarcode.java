367334fce56234568e3fd19035c58be6b9c6a84c
Sekai422 <36085216+Sekai422@users.noreply.github.com>
2019-02-17 06:36:42 -0700

    update mah branch (Jin)

--- app/src/main/java/com/plastic/plastico/NoBarcode.java
+++ app/src/main/java/com/plastic/plastico/NoBarcode.java
@@ -2,12 +2,35 @@ package com.plastic.plastico;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import com.firebase.client.Firebase;
 
 public class NoBarcode extends AppCompatActivity {
+
+    private EditText nBarcode;
+    private Button nSendData;
+
+    private Firebase nRef;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_no_barcode);
+
+
+        nRef = new Firebase("https://plasteco-3a8bc.firebaseio.com/objects");
+        nBarcode = (EditText) findViewById(R.id.barcode);
+        nSendData = (Button) findViewById(R.id.sendData);
+        nSendData.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String barcode = nBarcode.getText().toString();
+                Firebase nRefChild = nRef.child("Name");
+                nRefChild.setValue("barcode");
+            }
+        });
     }
 }
