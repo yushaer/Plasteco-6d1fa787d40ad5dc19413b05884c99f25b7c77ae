344ed4a46b08bb15ef25e8f0cecebe110cb8542a
Sekai422 <36085216+Sekai422@users.noreply.github.com>
2019-02-16 23:36:55 -0700

    Fixed layout for no Barcode activity

--- app/src/main/java/com/plastic/plastico/MainActivity.java
+++ app/src/main/java/com/plastic/plastico/MainActivity.java
@@ -16,8 +16,6 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-
-
     }
 
     public void login(View view){
