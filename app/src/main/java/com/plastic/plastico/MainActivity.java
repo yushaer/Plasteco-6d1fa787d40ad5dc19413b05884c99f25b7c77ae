0cc15a7ef7def8e082c18afd72950f1570c033e2
yushaefraza@gmail.com <yushaefraza@gmail.com>
2019-02-16 18:33:29 -0700

    Initial commit

---
+++ app/src/main/java/com/plastic/plastico/MainActivity.java
@@ -0,0 +1,26 @@
+package com.plastic.plastico;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.content.Intent;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+    }
+    public void scanbarcode(View view)
+        {
+                Intent qrscan = new Intent(MainActivity.this,getbarcode.class);
+        }
+
+}
